63ea3a8b715d50b1eb7e13aaca1702a1bf7da9d0
coder9430 <cse21bcsc01@silicon.ac.in>
2024-12-15 10:44:52 +0530

    basic structure

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+import { Button } from './components/ui/button'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='px-20'>
+       <Button>Hello</Button>
+     </div>
+    </>
+  )
+}
+
+export default App
